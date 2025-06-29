bab0b41656eb927867b5d480cf50c60f84233925
Amir khodadi <amiramkhodadi@gmail.com>
2025-06-29 12:49:21 +0330

    Initial project setup with custom user and PostgreSQL config

--- learntiy/settings.py
+++ learntiy/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'learntiy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'learntiy_db',
+        'USER': 'learntiy_owner',
+        'PASSWORD': '12344321',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
